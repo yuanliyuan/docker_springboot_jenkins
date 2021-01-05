3d80500e312ef4d1fa6daaa7786a0cfc6dd5e9b2
yuanli <578598624@qq.com>
2021-01-05 21:10:42 +0800

    edit port

--- docker.sh
+++ docker.sh
@@ -2,5 +2,5 @@ mvn clean package  docker:build
 echo "当前docker 镜像："
 docker images
 echo "启动容器----->"
-docker run -p 8001:8001 -d dockerspringboot
+docker run -p 8989:8989 -d dockerspringboot
 echo "启动服务成功！"
